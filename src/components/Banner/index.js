5c2540b527c7c58d033c82ad4526e306e8de449a
Marcos Paulo dos Santos <marcos.dev.1990@gmail.com>
2023-09-01 08:39:11 -0300

    first commit

---
+++ src/components/Banner/index.js
@@ -0,0 +1,15 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img
+        src="/image/banner.png"
+        alt=" O banner principal da pagina do Organo"
+      />
+    </header>
+  
+  );
+}
+
+export default Banner;
